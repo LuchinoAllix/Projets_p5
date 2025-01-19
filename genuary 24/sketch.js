9df64a6ce12a104043dc23a43cc3243d233e8fa4
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2025-01-19 10:31:22 -0500

    genuary 24 challenge

---
+++ genuary 24/sketch.js
@@ -0,0 +1,36 @@
+const side = 800;
+const nb = 15; // nombre de carré par côté
+const rap = side / nb; // taille de chaque carré
+const rap2 = rap * 0.5; // taille de chaque carré / 2
+
+function setup() {
+  createCanvas(side, side);
+  colorMode(HSB, 360, 100, 100, 250);
+  noLoop();
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background("white");
+  // on prend tous les points de la grille
+  for (i = 0; i < nb; i++) {
+    for (j = 0; j < nb; j++) {
+      skwair(i * rap, j * rap);
+    }
+  }
+}
+
+function skwair(x, y) {
+  push();
+  rand = random();
+  translate(x + rap2, y + rap2);
+  rotate(rand * 45);
+  translate(-x - rap2, -y - rap2);
+  fill(120, 100 * (1 - rand), 100 * (1 - rand));
+  noStroke();
+  rect(x, y, rap);
+  pop();
+}
+
+// Rotation :
+// https://stackoverflow.com/questions/65900807/how-do-i-rotate-a-rectangle-shape-from-a-specific-point-in-p5-js

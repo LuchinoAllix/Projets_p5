da8d49d06c09ba89edbcde6f9e1d58d9bb3f3f25
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2025-01-23 13:44:24 -0500

    Update sketch.js

--- hexa_maze/sketch.js
+++ hexa_maze/sketch.js
@@ -1,6 +1,6 @@
 const side = 800;
 const nb = 21;
-const apo_thick = side / ( 2 * nb);
+const apo_thick = side / (2 * nb);
 const thick = 3;
 const apo = apo_thick - thick;
 const sq3 = Math.sqrt(3);
@@ -9,7 +9,7 @@ const radius_thick2 = radius_thick / 2;
 const radius = 2 * apo / sq3;
 const radius2 = radius / 2;
 
-const nb_rows = 2*nb-1;
+const nb_rows = 2 * nb - 1;
 const nb_cols = nb
 
 const side_color = "gold";
@@ -18,52 +18,67 @@ var start_color_index = 0
 
 let grid = new Array(nb_rows).fill().map(() => new Array(nb_cols).fill());
 
-var start;
-var finish;
-var stack2;
-var finished = false;
-var init = false
+let start;
+let finish;
+let stack2 = [];
+let finished = false;
+let init = false
 
 function setup() {
   createCanvas(side, side);
   colorMode(HSB, 360, 100, 100, 250)
   frameRate(2);
   //noLoop();
-  
-  
 }
 
-function draw() { // to make work
-  if (!init){
-    make_grid()
-    make_maze()
-    draw_grid()
-    start = grid[0][0];
-    finish = grid[nb_rows-1][nb_cols-1];
-    stack2 = [start];
-    console.log(start)
-    console.log(stack2)
-    draw_inner_hex(start);
-    init = true;
+function draw() {
+  console.log(0);
+  if (!init) {
+    initiate();
+  } else {
+    solve(); // pourquoi est-ce que stack change avant d'arriver là ?????
   }
-  if(start_color_index > 1){yay}
+}
+
+function initiate(){
+  console.log("initiation");
+  make_grid();
+  make_maze();
+  draw_grid();
+
+  start = grid[0][0];
+  finish = grid[nb_rows - 1][nb_cols - 1];
+
+  console.log(start);
+  stack2.push(start);
+  console.log(stack2);
+  
+  draw_inner_hex(start);
+  
+  init = true;
+}
+
+function solve(){
+  console.log("solving");
+  //if (start_color_index > 4) { yay1 }
   console.log("stack2 :")
   console.log(stack2)
-  if (finished){ yay } else {
+  if (finished) { yay2 } else {
+    console.log(3);
     stack2[0].explored = true;
     draw_inner_hex(stack2[0]);
     stack2[0].links.forEach(link => {
       console.log(link)
       var v = grid[link[0]][link[1]];
-      if (v.i == finish.i && v.j == finish.j){finished=true}
-      if ( ! v.explored ){
+      if (v.i == finish.i && v.j == finish.j) { finished = true }
+      if (!v.explored) {
         stack2.push(v);
-      } 
+      }
     })
-    stack2.splice(0,1);
-  } 
-  if (stack2.length == 0){
-    yay
+    stack2.splice(0, 1);
+  }
+  if (stack2.length == 0) {
+    yay3
   }
 }
 
@@ -85,19 +100,19 @@ function drawHex(hex) {
   }
 }
 
-function draw_inner_hex(hex){
+function draw_inner_hex(hex) {
   const points = get_points(hex.x, hex.y);
-  fill(start_color_index,50,50);
+  fill(start_color_index, 50, 50);
   noStroke()
   beginShape();
-  vertex(points[1][0],points[1][1])
-  vertex(points[3][0],points[3][1])
-  vertex(points[5][0],points[5][1])
-  vertex(points[7][0],points[7][1])
-  vertex(points[9][0],points[9][1])
-  vertex(points[11][0],points[11][1])
+  vertex(points[1][0], points[1][1])
+  vertex(points[3][0], points[3][1])
+  vertex(points[5][0], points[5][1])
+  vertex(points[7][0], points[7][1])
+  vertex(points[9][0], points[9][1])
+  vertex(points[11][0], points[11][1])
   endShape();
-  start_color_index+=1;
+  start_color_index += 1;
 }
 
 function get_points(x, y) {
@@ -119,39 +134,39 @@ function get_points(x, y) {
 
 function make_grid() {
   for (var i = 0; i < nb_rows; i++) {
-    for (var j = (i%2); j < nb_cols; j+=2) {
-      grid[i][j] = make_hex(i,j);
+    for (var j = (i % 2); j < nb_cols; j += 2) {
+      grid[i][j] = make_hex(i, j);
     }
   }
   for (var i = 0; i < nb_rows; i++) {
-    for (var j = (i%2); j < nb_cols; j+=2) {
-      if (i-1 >= 0){
-        if (j-1 >= 0){
-          grid[i][j].links.push([i-1,j-1])
-        } if (j+1 <  nb_cols ){
-          grid[i][j].links.push([i-1,j+1])
+    for (var j = (i % 2); j < nb_cols; j += 2) {
+      if (i - 1 >= 0) {
+        if (j - 1 >= 0) {
+          grid[i][j].links.push([i - 1, j - 1])
+        } if (j + 1 < nb_cols) {
+          grid[i][j].links.push([i - 1, j + 1])
         }
       }
-      if (i-2 >= 0){
-        grid[i][j].links.push([i-2,j])
+      if (i - 2 >= 0) {
+        grid[i][j].links.push([i - 2, j])
       }
-      if (i+1 <  nb_rows ){
-        if (j-1 >= 0){
-          grid[i][j].links.push([i+1,j-1])
-        } if (j+1 < nb_cols){
-          grid[i][j].links.push([i+1,j+1])
-        }  
+      if (i + 1 < nb_rows) {
+        if (j - 1 >= 0) {
+          grid[i][j].links.push([i + 1, j - 1])
+        } if (j + 1 < nb_cols) {
+          grid[i][j].links.push([i + 1, j + 1])
+        }
       }
-      if (i+2 < nb_rows){
-        grid[i][j].links.push([i+2,j])
+      if (i + 2 < nb_rows) {
+        grid[i][j].links.push([i + 2, j])
       }
     }
   }
 }
 
-function draw_grid(){
+function draw_grid() {
   for (var i = 0; i < nb_rows; i++) {
-    for (var j = (i%2); j < nb_cols; j+=2) {
+    for (var j = (i % 2); j < nb_cols; j += 2) {
       drawHex(grid[i][j]);
 
     }
@@ -160,9 +175,9 @@ function draw_grid(){
 
 function make_hex(i, j) {
   return {
-    i:i,
-    j:j,
-    x: (i+1) *apo_thick,
+    i: i,
+    j: j,
+    x: (i + 1) * apo_thick,
     y: apo_thick * 2 / sq3 + j * (apo_thick * 2 / sq3 + radius_thick2),
     walls: [true, true, true, true, true, true],
     links: [],
@@ -171,60 +186,60 @@ function make_hex(i, j) {
   }
 }
 
-function make_maze(){
-  var a = Math.floor(nb_rows /2)
-  if (a%2){a+=1}
-  var stack = [grid[a][a-2]]
-  while (stack.length > 0){
-    var current_cell = stack[stack.length-1]
+function make_maze() {
+  var a = Math.floor(nb_rows / 2)
+  if (a % 2) { a += 1 }
+  var stack = [grid[a][a - 2]]
+  while (stack.length > 0) {
+    var current_cell = stack[stack.length - 1]
     current_cell.visited = true;
     var next_cell_index = get_random_unvisited_neighbour(current_cell);
-    if (next_cell_index == -1){
+    if (next_cell_index == -1) {
       stack.pop()
     } else {
-      open(current_cell,next_cell_index);
+      open(current_cell, next_cell_index);
       var l = current_cell.links[next_cell_index]
       stack.push(grid[l[0]][l[1]]);
     }
   }
 }
 
-function get_random_unvisited_neighbour(hex){
+function get_random_unvisited_neighbour(hex) {
   var fails = 0;
-  var n = Math.floor(random(0,hex.links.length));
-  while (fails < hex.links.length){
+  var n = Math.floor(random(0, hex.links.length));
+  while (fails < hex.links.length) {
     var l = hex.links[n]
-    if (grid[l[0]][l[1]].visited == false){
+    if (grid[l[0]][l[1]].visited == false) {
       return n;
     } else {
-      n = (n+1)%hex.links.length
-      fails +=1
+      n = (n + 1) % hex.links.length
+      fails += 1
     }
   }
   return -1
 }
 
-function open(hex,n){
+function open(hex, n) {
   var index = hex.links[n]
   var door = -1;
-  if(index[0]==hex.i+1){
-    if (index[1]==hex.j+1){
+  if (index[0] == hex.i + 1) {
+    if (index[1] == hex.j + 1) {
       door = 3;
-    } else if (index[1]==hex.j-1) {
+    } else if (index[1] == hex.j - 1) {
       door = 1;
     }
-  } else if ( index[0] == hex.i-2 && index[1] == hex.j){
+  } else if (index[0] == hex.i - 2 && index[1] == hex.j) {
     door = 5;
-  } else if ( index[0] == hex.i+2 && index[1] == hex.j){
+  } else if (index[0] == hex.i + 2 && index[1] == hex.j) {
     door = 2;
-  } else if(index[0]==hex.i-1){
-    if (index[1]==hex.j+1){
+  } else if (index[0] == hex.i - 1) {
+    if (index[1] == hex.j + 1) {
       door = 4;
-    } else if (index[1]==hex.j-1) {
+    } else if (index[1] == hex.j - 1) {
       door = 0;
     }
   }
   hex.walls[door] = false;
   var l = hex.links[n]
-  grid[l[0]][l[1]].walls[(door+3)%6] = false
+  grid[l[0]][l[1]].walls[(door + 3) % 6] = false
 }

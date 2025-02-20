5454499ce3ede0074a83775ab61b2776565b8670
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2025-02-20 13:56:47 -0500

    ineraction api open-meteo

---
+++ test__opendata/README.md
@@ -0,0 +1,4 @@
+
+[open-meteo.com](https://open-meteo.com/)
+
+[Documentation API](https://open-meteo.com/en/docs)
--- test__opendata/sketch.js
+++ test__opendata/sketch.js
@@ -1,1 +1,46 @@
 
+// URL de l'API Open-Meteo
+const url = "https://api.open-meteo.com/v1/forecast";
+
+// Paramètres de la requête
+const params = {
+    "latitude": 45.4215,
+    "longitude": -75.6972,
+    "current": [
+        "temperature_2m",
+        "relative_humidity_2m",
+        "apparent_temperature",
+        "is_day",
+        "precipitation",
+        "rain",
+        "cloud_cover",
+        "wind_speed_10m",
+        "wind_direction_10m"
+    ],
+    "hourly": "temperature_2m"
+};
+
+let data_set;
+
+function makeURL(url, params){
+	const queryString = new URLSearchParams(params).toString();
+    return `${url}?${queryString}` ;
+}
+
+async function getData(url){
+	try {
+		const rep = await fetch(url)
+  		const data = await rep.json()
+		data_set = data.current;
+		console.log(data);
+	} catch(error) {
+		console.error("Erreur lors de la récupération des données météo:", error)
+	}
+}
+
+async function run(){
+	await getData(makeURL(url,params))
+	console.log(data_set);
+}
+
+run()

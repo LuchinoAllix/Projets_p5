5a3c59d19f4917ca81313e13cc2d207cc01ba1f9
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2025-02-26 14:44:04 -0500

    skwèrz

--- test__opendata/sketch.js
+++ test__opendata/sketch.js
@@ -20,7 +20,64 @@ const params = {
     "hourly": "temperature_2m"
 };
 
-let data_set;
+// variable pour données météo
+let data_set =  { time: "2025-02-20T19:00", interval: 900, temperature_2m: -7, relative_humidity_2m: 79, apparent_temperature: 49, is_day: 1, precipitation: 0, rain: 0, cloud_cover: 100, wind_speed_10m: 11.5, wind_direction_10m:302}
+
+const minTemp = -50;  // Température très froide
+const maxTemp = 50;   // Température très chaude
+
+// w & h of canva
+const side = 800; 
+
+function setup(){
+	createCanvas(side, side);
+  	noLoop()
+	frameRate(10)
+  	angleMode(DEGREES);
+	colorMode(HSB, 360, 100, 100, 250)
+
+}
+
+let nb_squares = 10
+let size_square = side / nb_squares
+let rec = 27
+let colors = ["black","white","purple"]
+let index = 0
+let factor = 2.5
+
+function draw(){
+	background("white")
+	draw_big()
+}
+
+function draw_big(){
+	for(i=0;i< nb_squares;i++){
+		for(j=0;j< nb_squares;j++){
+			draw_squares(i*size_square,j*size_square)
+		}
+	}
+}
+
+function draw_squares(i,j){
+	let a = 0
+	let size_inner_square = size_square
+	index = 0
+	while (size_inner_square > 0){
+	let rx = random([0,1])
+	let ry = random([0,1])
+		let diff = [0,size_square-size_inner_square]
+		push()
+		translate(diff[rx],diff[ry])
+		fill(colors[index])
+		square(i,j,size_inner_square)
+		console.log(size_inner_square)
+		index = (index+1)%colors.length
+		size_inner_square = size_square-(random(1.05,1.15)*a)
+		a++
+		pop()
+	}
+}
+
 
 function makeURL(url, params){
 	const queryString = new URLSearchParams(params).toString();
@@ -43,4 +100,23 @@ async function run(){
 	console.log(data_set);
 }
 
-run()
+function getColorFromTemp(temp) {
+	let H = 0
+	let S = 100
+	let B = 100
+	let x = temp + 50
+	if (x<50){
+		H = 250 - 0.9 * x 
+		B = 100 - x
+		S = 100 - x
+	} else {
+		H = 60 - temp -10
+		B = x
+		S = x 
+	}
+	console.log(H)
+    return [H,S,B]
+}
+
+
+//run()

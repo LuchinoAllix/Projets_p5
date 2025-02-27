f3eb4e02523862d45a417ed8e2588f0dd55cc9af
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2025-02-26 21:11:42 -0500

    cleaned code

--- test__opendata/sketch.js
+++ test__opendata/sketch.js
@@ -4,70 +4,58 @@ const url = "https://api.open-meteo.com/v1/forecast";
 
 // Paramètres de la requête
 const params = {
-    "latitude": 45.4215,
-    "longitude": -75.6972,
+    "latitude": 0,
+    "longitude": 0,
     "current": [
         "temperature_2m",
-        "relative_humidity_2m",
-        "apparent_temperature",
-        "is_day",
-        "precipitation",
-        "rain",
-        "cloud_cover",
-        "wind_speed_10m",
         "wind_direction_10m"
-    ],
-    "hourly": "temperature_2m"
+    ]
 };
 
-// variable pour données météo
-let data_set =  { time: "2025-02-20T19:00", interval: 900, temperature_2m: 40, relative_humidity_2m: 79, apparent_temperature: 49, is_day: 1, precipitation: 0, rain: 0, cloud_cover: 100, wind_speed_10m: 11.5, wind_direction_10m:302}
-
 // w & h of canva
-const side = 800; 
+const side = 600; 
+
+// nombre de carrées sur un côté
+let nb_squares = 10
+let size_square = side / nb_squares
+let colors = ["black","white","white"]
+let index = 0
 
 function setup(){
 	createCanvas(side, side);
   	noLoop()
-	frameRate(10)
-  	angleMode(DEGREES);
 	colorMode(HSB, 360, 100, 100, 250)
-
 }
-
-let nb_squares = 10
-let size_square = side / nb_squares
-let colors = ["black","white",getColorFromTemp(data_set.temperature_2m)]
-let index = 0
 
 function draw(){
 	background("white")
-
 	draw_big()
 }
 
-function draw_big(){
+async function draw_big(){
 	for(i=0;i< nb_squares;i++){
 		for(j=0;j< nb_squares;j++){
-			draw_squares(i*size_square,j*size_square)
+			let data = await getData(makeURL(url,5*i,5*j,params));
+			if(data === undefined){
+				data = {wind_direction:0,temperature_2m:0}
+			}
+			draw_squares(i*size_square,j*size_square,data.wind_direction_10m,data.temperature_2m)
 		}
 	}
 }
 
-function draw_squares(i,j){
+function draw_squares(i,j,angle,temp){
 	let a = 0
 	let size_inner_square = size_square
 	index = 0
-	let rx = random([0,1])
-	let ry = random([0,1])
-
+	let pair = getDirFromAngle(angle)
+	colors[colors.length-1] = getColorFromTemp(temp)
 	while (size_inner_square > 0){
 		let diff = size_square-size_inner_square
 		push()
-		translate(rx*diff,ry*diff)
+		translate(pair[0]*diff,pair[1]*diff)
 		fill(colors[index])
 		square(i,j,size_inner_square)
-		console.log(size_inner_square)
 		index = (index+1)%colors.length
 		size_inner_square = size_square-(random(2,2.5)*a)
 		a++
@@ -75,8 +63,9 @@ function draw_squares(i,j){
 	}
 }
 
-
-function makeURL(url, params){
+function makeURL(url,lat,long, params){
+	params.latitude = lat;
+	params.longitude = long;
 	const queryString = new URLSearchParams(params).toString();
     return `${url}?${queryString}` ;
 }
@@ -85,16 +74,10 @@ async function getData(url){
 	try {
 		const rep = await fetch(url)
   		const data = await rep.json()
-		data_set = data.current;
-		console.log(data);
+		return  data.current;
 	} catch(error) {
 		console.error("Erreur lors de la récupération des données météo:", error)
 	}
-}
-
-async function run(){
-	await getData(makeURL(url,params))
-	console.log(data_set);
 }
 
 function getColorFromTemp(temp) {
@@ -107,7 +90,6 @@ function getColorFromTemp(temp) {
 		H = 245
 		S = -4*temp 
 	}
-	console.log(H)
     return [H,S,B]
 }
 
@@ -121,5 +103,3 @@ function getDirFromAngle(angle){
 	}
 	return [0,0]
 }
-
-//run()
